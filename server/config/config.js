4b27a434ae3dc7cfcafd380adf7c87a536a8a0c1
GOlmedoFsa <gustavo.olmedo.formosa@gmail.com>
2017-08-10 16:38:42 -0300

    Add tests for GET /users/me and POST /users

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,6 @@ if(env === 'development') {
     console.log(process.env.PORT);
     console.log('******************');
 } else if (env === 'test'){
-    process.env.PORT = 3000;
+    process.env.PORT = 4000;
     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';    
 }
---
+++ server/tests/seed/seed.js
@@ -0,0 +1,46 @@
+const { ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
+
+const userOneId = new ObjectId();
+const userTwoId = new ObjectId();
+const users = [{
+    _id: userOneId,
+    email: 'test@mail.com',
+    password: 'userOnePass',
+    tokens:[{
+        access: 'auth',
+        token: jwt.sign({_id:userOneId,access:'auth'},'abc123').toString()
+    }]
+}, {
+    _id: userTwoId,
+    email: 'tt@mail.com',
+    password: 'userTwoPass'
+}];
+
+const todos = [{
+    _id: new ObjectId(),
+    text:'First test todo'
+},{
+    _id: new ObjectId(),
+    text:'Second test todo',
+    completed: true,
+    completedAt:333
+}];
+
+const populateTodos = (done)=>{
+    Todo.remove({}).then(()=>{
+        return Todo.insertMany(todos);
+    }).then(()=> done());
+};
+
+const populateUsers = (done)=>{
+    User.remove({}).then(()=>{
+        const userOne = new User(users[0]).save();
+        const userTwo = new User(users[1]).save();
+
+        return Promise.all([userOne,userTwo]);
+    }).then(()=> done());
+}
+module.exports = { todos, populateTodos, users, populateUsers };
