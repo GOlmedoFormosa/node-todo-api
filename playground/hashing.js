d2291f017e08c11fc35b559bc1ac857c9cf83225
GOlmedoFsa <gustavo.olmedo.formosa@gmail.com>
2017-08-10 15:37:24 -0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,6 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcryptjs');
 // const message = 'I am user number 3';
 // const  hash = SHA256(message).toString();
 // console.log(`Message; ${message}`);
@@ -30,11 +30,24 @@ const jwt = require('jsonwebtoken');
 //jwt.sign to create
 //jwt.verify
 
-const data = {
-    id:10
-}
-const token = jwt.sign(data,'123abc');
-console.log(token);
+// const data = {
+//     id:10
+// }
+// const token = jwt.sign(data,'123abc');
+// console.log(token);
 
-const decoded = jwt.verify(token,'123abc');
-console.log('decoded ',decoded);
+// const decoded = jwt.verify(token,'123abc');
+// console.log('decoded ',decoded);
+//---------------------------------------------
+const password = '123abc!';
+// bcrypt.genSalt(10,(err,salt)=>{
+//     bcrypt.hash(password, salt,(err,hash)=>{
+//         console.log(hash);
+//     });
+// });
+
+
+const hashedPassword = 'asdfasdf';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    console.log(res);
+});
