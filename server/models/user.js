296787180113532c27782363c07ea0e893321201
GOlmedoFsa <gustavo.olmedo.formosa@gmail.com>
2017-08-11 00:58:40 -0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -62,7 +62,16 @@ UserSchema.methods.generateAuthToken = function(){
     });
 };
 
-UserSchema.statics.findByToken = function(token){
+UserSchema.methods.removeToken = function(token) {
+    const user = this;
+    return user.update({
+        $pull:{
+            tokens: { token }
+        }
+    });
+}
+
+UserSchema.statics.findByToken = function(token) {
     const User = this;
     let decoded;
     try{
